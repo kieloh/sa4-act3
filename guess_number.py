a006a12f706429116b4185b32c2800f513bad396
kieloh <nathan563412@gmail.com>
2024-02-07 22:36:01 -0600

    Added hints to tell whether guess is higher or lower than the static number

--- guess_number.py
+++ guess_number.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python3
 
-number = '10'
+number = 10
 guess = '0'
 
-
+   
 
 while guess != 'q':
     
-
-    print("I'm thinking of a number...")
-    guess = input("Enter what number I am thinking of, or enter 'q' to quit: ")
-    
-    if guess == number:
-        print("Congratulations! You guessed the right number.")
-        guess = 'q'
-    elif guess == 'q':
-        guess = 'q'
-    else:
-        print("Wrong, Try again.")
+    try:
+        print("I'm thinking of a number...")
+        guess = input("Enter what number I am thinking of, or enter 'q' to quit: ")
+        
+        
+        ng = int(guess)
 
     
+        if ng == number:
+            print("Congratulations! You guessed the right number.")
+            guess = 'q'
+        
+        elif ng > number:
+            print("Wrong, Guess is too high. Try again.")
+        elif ng < number:
+            print("Wrong, Guess is too Low. Try agian.")
+
+        
+    except:
+        if guess == 'q':
+            guess = 'q'
+        else:
+            "Please enter a number or q"
+    
     
 
-
+    
 
 print(f"The number was {number}.")
