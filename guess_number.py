e63d7ded19948bef5ca14bfdf85e46d04633cb44
kieloh <nathan563412@gmail.com>
2024-02-07 21:47:54 -0600

    Takes user input to guess a hardcoded number indefinately until user enters 'q' to stop

--- guess_number.py
+++ guess_number.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
 
-number = 10
+number = '10'
+guess = '0'
 
-print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
 
-if guess == number:
-    print("Congratulations! You guessed the right number.")
-else:
-    print(f"Sorry! The number was {number}.")
+
+while guess != 'q':
+    
+
+    print("I'm thinking of a number...")
+    guess = input("Enter what number I am thinking of, or enter 'q' to quit: ")
+    
+    if guess == number:
+        print("Congratulations! You guessed the right number.")
+        guess = 'q'
+    elif guess == 'q':
+        guess = 'q'
+    else:
+        print("Wrong, Try again.")
+
+    
+    
+
+
+
+print(f"The number was {number}.")
